90e41662ddfbcbcd033c13c8c42681aaf68b618b
James Benjamin <james.abenj@gmail.com>
2020-03-13 21:22:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/pig_latinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @user_phrase = params[:user_phrase]
+    @analyze_text = PigLatinizer.new
+    erb :result
+  end
 end
---
+++ models/pig_latinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+
+  def piglatinize(phrase)
+    ay = "ay"
+    way = "way"
+    pig_array = phrase.split.map do |word|
+      if word[0].match(/[aeiouyAEIOUY]/)
+        new_word = word + way
+        new_word
+      else
+        initial_consonants = word.slice!(/^([^aeiouy]+)/)
+        new_word = word + initial_consonants + ay
+        new_word
+      end
+    end
+    pig_array.join(" ")
+  end
+
+end
